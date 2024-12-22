9235a7deb7a33b82b14a47eb2751ceb1be1daa5e
Chirag Joshi <chirag45610@gmail.com>
2024-12-22 21:54:50 +0530

    Removed API key

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,26 +3,24 @@ import cohere
 
 app = Flask(__name__)
 
-# Initialize Cohere client with your API key
-co = cohere.ClientV2("JNa8ZIU2UUbErgtn8kqGyPUSvkqso5O6YdDDpxYb")
+co = cohere.ClientV2("")
 
 @app.route('/send-text', methods=['POST'])
 def send_text():
-    data = request.get_json()  # Get JSON data from React frontend
-    user_message = data.get('text')  # Extract text from JSON
+    data = request.get_json()  
+    user_message = data.get('text')  
 
     try:
-        # Make a call to Cohere's chat API
+        
         response = co.chat(
             model="command-r-plus",
             messages=[{"role": "user", "content": user_message}]
         )
 
-        # Extract and send the content from the response
         return jsonify({"response": response['messages'][0]['content']})
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500  # Handle errors if any
+        return jsonify({"error": str(e)}), 500  
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # Run Flask on port 5000
+    app.run(debug=True, port=5000)  
