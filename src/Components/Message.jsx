4bd34007d37c7ca1a9744b745472618b41d92b7f
Chirag Joshi <chirag45610@gmail.com>
2025-01-01 00:18:24 +0530

    Message Box Designed

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -5,8 +5,14 @@ import ReactMarkdown from 'react-markdown';
 
 const Message = ({ text, isUserMessage }) => {
     return (
-        <div className={`text-white flex text-lg p-3 rounded-lg mb-2 ${isUserMessage ? 'bg-gray-700' : 'bg-gray-800'}`}>
-            <img src={isUserMessage ? user : bot} alt="Avatar" className="w-8 h-8 rounded-full mr-3 " />
+        <div
+            className={`text-white flex text-lg p-3 rounded-lg mb-2 ${isUserMessage ? 'bg-gray-700' : 'bg-gray-800'}`}
+        >
+            <img
+                src={isUserMessage ? user : bot}
+                alt="Avatar"
+                className={`w-8 h-8 rounded-full mr-3 ${isUserMessage ? '' : 'pt-1'}`}
+            />
             <div className="flex flex-col">
                 <ReactMarkdown>{text}</ReactMarkdown>
             </div>
